25d83b1fd1fd343eb1eb11bd7102f73223c57745
nimbleninja <zaporozhets93@gmail.com>
2024-11-05 23:27:10 +0200

    update fonts preload

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
-import { Inter, Itim, Lusitana } from "next/font/google";
+import { Inter, Itim, Lusitana } from 'next/font/google'
 
-export const inter = Inter({ subsets: ["latin"] });
-export const itim = Itim({ weight: "400" });
+export const inter = Inter({ subsets: ['latin'] })
+export const itim = Itim({ weight: '400', subsets: ['vietnamese'] })
 export const lusitana = Lusitana({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
